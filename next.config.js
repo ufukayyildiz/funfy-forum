0fd671f9e62b3a7ce0024757daffe9061784675b
Ufuk Ayyıldız <23309320+ufukayyildiz@users.noreply.github.com>
2024-07-17 02:39:09 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -12,6 +12,13 @@ const nextConfig = {
         destination: '/topic/:id/new',
         permanent: true,
       },
+
+      module.exports = {
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL= 'https://mymszlfpxqgqgxocdopb.supabase.co',
+    NEXT_PUBLIC_SUPABASE_ANON_KEY= 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im15bXN6bGZweHFncWd4b2Nkb3BiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA4Mjk0NzAsImV4cCI6MjAzNjQwNTQ3MH0.onp7rgtKEQwlTIBE0GWybfu1auatpKKWT-38vqJF9zY',
+  },
+},
     ];
   },
 };
